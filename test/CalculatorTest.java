e19ae24ad8f6322c28db6e6f58be655a49e2adb6
Yota Adachi <yota961219@gmail.com>
2024-07-20 17:45:34 +0900

    Practice

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -2,17 +2,26 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
 	@Nested
 	public class taxPriceTest {
+
+		Calculator cal;
+
+		@BeforeEach
+		public void Setup() {
+			// オブジェクトの生成
+			cal = new Calculator();
+		}
+
 		@Test
 		public void testTaxPrice() {
-			// オブジェクトの生成
-			Calculator cal = new Calculator();
 			// 期待値
 			int expected = 110;
 			// 実測値
@@ -22,9 +31,8 @@ public class CalculatorTest {
 		}
 
 		@Test
+		@Tag("Exception")
 		public void testTaxPriceException() {
-			// オブジェクトの生成
-			Calculator cal = new Calculator();
 			// 実行
 			assertThrows(IllegalArgumentException.class, () -> {
 				cal.taxPrice(-100);
