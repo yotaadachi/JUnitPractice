f5596ca81565fb389d78918fe3f1b813283a8683
Yota Adachi <yota961219@gmail.com>
2024-07-20 17:32:06 +0900

    Practice

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -2,30 +2,33 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
-	@Test
-	public void testTaxPrice() {
-		// オブジェクトの生成
-		Calculator cal = new Calculator();
-		// 期待値
-		int expected = 110;
-		// 実測値
-		int actual = cal.taxPrice(100);
-		// 比較
-		assertEquals(expected, actual);
-	}
+	@Nested
+	public class taxPriceTest {
+		@Test
+		public void testTaxPrice() {
+			// オブジェクトの生成
+			Calculator cal = new Calculator();
+			// 期待値
+			int expected = 110;
+			// 実測値
+			int actual = cal.taxPrice(100);
+			// 比較
+			assertEquals(expected, actual);
+		}
 
-	@Test
-	public void testTaxPriceException() {
-		// オブジェクトの生成
-		Calculator cal = new Calculator();
-		// 実行
-		assertThrows(IllegalArgumentException.class, () -> {
-			cal.taxPrice(-100);
-		});
+		@Test
+		public void testTaxPriceException() {
+			// オブジェクトの生成
+			Calculator cal = new Calculator();
+			// 実行
+			assertThrows(IllegalArgumentException.class, () -> {
+				cal.taxPrice(-100);
+			});
+		}
 	}
-
 }
